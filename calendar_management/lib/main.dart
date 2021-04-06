81bfb42e53a6499af51ea3f7eb39426321b1ddb2
srujit-n <srujit2001@gmail.com>
2021-04-06 19:55:09 +0530

    calendar skeleton code updated .. modifications tomorrow

--- calendar_management/lib/main.dart
+++ calendar_management/lib/main.dart
@@ -6,7 +6,7 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget{
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
