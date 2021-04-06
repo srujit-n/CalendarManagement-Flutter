8c986c7ee4ddbe64f55ed94cd0f9d5894f819901
srujit-n <srujit2001@gmail.com>
2021-04-06 20:18:18 +0530

    integrated firebase into the app ,authentication still need to be done

--- calendar_management/lib/main.dart
+++ calendar_management/lib/main.dart
@@ -1,30 +1,49 @@
 import 'package:flutter/material.dart';
 
+// Import the firebase_core plugin
+import 'package:firebase_core/firebase_core.dart';
+import 'package:overlay_support/overlay_support.dart';
+
 import 'calendar.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget{
-  // This widget is the root of your application.
+class MyApp extends StatelessWidget {
+  // Create the initialization Future outside of `build`:
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: TableEventsExample(),
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          return CircularProgressIndicator();
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return OverlaySupport(
+              child: MaterialApp(
+                  debugShowCheckedModeBanner: false,
+                  title: 'TrashToCash',
+                  theme: ThemeData(
+                    fontFamily: 'Nunito',
+                    primaryColor: const Color(0xff29a39d),
+                    accentColor: const Color(0xff29a39d),
+                  ),
+                  home: TableEventsExample()
+              ));
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return CircularProgressIndicator();
+      },
     );
   }
 }
